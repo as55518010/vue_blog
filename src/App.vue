8a50527bd7c1263642db1d4bd3f54d28dced4aea
Derrick <as55518010@yahoo.com.tw>
2020-11-25 13:40:00 +0000

    初始化項目 導入 bootstrap css ui

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <column-list :list="list"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的專欄',
+    description: 'saddsadas',
+    avatar: 'https://cdn.mos.cms.futurecdn.net/zJ5DMr39mEbFYAyandCBMj-1200-80.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的專欄',
+    description: '專專專專專',
+    avatar: 'https://imgix.bustle.com/uploads/image/2020/8/20/6e6407d6-438d-4ad4-b3d6-e18acec04d7e-avatar-tla.jpg?w=350&h=298&auto=format%2Ccompress&cs=srgb&q=70&fit=crop&crop=focalpoint&fp-x=0.5280172413793104&fp-y=0.46551724137931033'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="colum in list" :key="colum.id">
+      <img :src="colum.avatar" :alt="colum.title">
+      <h5>{{colum.title}}</h5>
+      <p>{{colum.description}}</p>
+      <a href="#">進入專欄</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps{
+    id: number;
+    title: string;
+    avatar: string;
+    description: string;
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+})
+</script>
